f01f0567718aeefb38300e585aeae0a1d4198a02
tungrg <tungrg07@gmail.com>
2025-05-23 17:49:53 +0700

    2nd commit

--- Program.cs
+++ Program.cs
@@ -10,7 +10,8 @@ namespace Slide3
     {
         
         public static void Main(string[] args) {
-            CreateFileWithData();
+            //CreateFolder();
+            CopyTest();
         }
         public static void CreateFile()
         {
@@ -32,6 +33,92 @@ namespace Slide3
                 //Ham write de ghi du lieu
                 F.Write(buffer);
             }
+        }
+        public static void ReadFile()
+        {
+            string path = "..\\..\\..\\data.txt";
+            using (FileStream fr = new FileStream(path, FileMode.Open, FileAccess.Read)) {
+                //tao 1 mang du lieu de luu noi dung cua file can doc
+                byte[] data = new byte[fr.Length];
+                // Doc noi dung file
+                fr.Read(data, 0, data.Length);
+                //Chuyen tu byte sang string de ghi ra man hinh
+                string dataToString = Encoding.UTF8.GetString(data);
+                Console.WriteLine(dataToString);
+            }
+        }
+        public static void TextWriteFile()
+        {
+            string path = "..\\..\\..\\textWriter.txt";
+            string data = "hello hello\n1234\n99999";
+            using (TextWriter tw = new StreamWriter(path)) {
+                tw.WriteLine(data);
+            }
+        }
+        public static void TextReadFile()
+        {
+            string path = "..\\..\\..\\textWriter.txt";
+            using (StreamReader sr = new StreamReader(path)) {
+                string data = sr.ReadToEnd();
+                Console.WriteLine(data);
+            }
+        }
+        public static void BinaryWriteFile()
+        {
+            string path = "..\\..\\..\\binaryWriter.bin";
+            //BinaryWriter su dung doi tuong stream de khoi tao
+            // --> Su dung File ~ Stream
+            using(BinaryWriter bw = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate)))
+            {
+                bw.Write(1.2f);
+                bw.Write(true);
+                bw.Write(false);
+            }
+        }
+        //Ghi xuong nhu nao
+        //Doc len nhu vay
+        public static void BinaryReadFile()
+        {
+            string path = "..\\..\\..\\binaryWriter.bin";
+            using (BinaryReader br = new BinaryReader(File.Open(path, FileMode.Open, FileAccess.Read)))
+            {
+                float a = br.ReadSingle();
+                bool b = br.ReadBoolean();
+                bool c = br.ReadBoolean();
+                Console.WriteLine($"{a} {b} {c}");
+            }
+        }
+        public static void CreateFolder()
+        {
+            string path = @"..\..\..\thu muc";
+            if (Directory.Exists(path) == false)
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+        public static void CopyAll(DirectoryInfo source, DirectoryInfo destination)
+        {
+            //Tao ten thuc muc cha
+            Directory.CreateDirectory(destination.FullName);
+            //Copy toan bo file trong thu muc hien dang dung vao thu muc dich
+            foreach (var i in source.GetFiles())
+            {
+                i.CopyTo(Path.Combine(destination.FullName, i.Name),true);
+            }
+            //Sao chep thu muc con
+            foreach(var j in source.GetDirectories())
+            {
+                var subFolder = destination.CreateSubdirectory(j.Name);
+                CopyAll(j,subFolder);
+            }
+        }
+        public static void CopyTest()
+        {
+            string path1 = @"..\..\..\thu muc";
+            string path2 = @"..\..\..\obj";
+            DirectoryInfo des = new DirectoryInfo(path1);
+            DirectoryInfo source = new DirectoryInfo(path2);
+            CopyAll(source, des);
         }
     }
 }
